f12afa09431bd33b18915e5ba046572ad7794048
TiziCode <mekhtoubtoufik@hotmail.com>
2025-06-26 23:31:09 +0200

    Correction : suppression infos sensibles

--- db_connection.py
+++ db_connection.py
@@ -1,18 +1,20 @@
 import pymysql
+from dotenv import load_dotenv
+import os
+
+# Charger les variables d'environnement depuis le fichier .env
+load_dotenv()
 
 # Fonction pour se connecter à la base de données MySQL via pymysql
 def get_db_connection():
     try:
-        # Connexion à la base de données MySQL
         connection = pymysql.connect(
-            host="localhost",  # Serveur local
-            user="root",  # Utilisateur MySQL
-            password="AbdToufik-mns",  # Mot de passe MySQL
-            database="projet"  # Nom de ta base de données
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
         )
         return connection
     except pymysql.MySQLError as e:
-        # Gestion des erreurs de connexion MySQL
         print(f"Erreur de connexion à la base de données MySQL: {e}")
-        raise  # On relance l'exception après l'avoir loguée
-
+        raise
